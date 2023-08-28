08b98c306264baf4a536909f2d925483f0c856a7
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-28 09:59:42 +0330

    some changes added to this project

---
+++ starter/assignments.js
@@ -0,0 +1,6 @@
+let country = "Iran";
+let continent = "Asia";
+let population = 87.92;
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ starter/script.js
@@ -0,0 +1,10 @@
+let js="amazing";
+    if(js==="amazing"){
+        alert("Js is Amazing!");
+    }
+console.log(21+53-24);
+let test="test text";
+let _function = "test";
+let PI=3.141595268;
+console.log(test);
+console.log(PI);
