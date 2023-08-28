439731876d5319f82e076c1699874949210b89d2
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-28 17:46:19 +0330

    some changes added to this project

--- starter/assignments.js
+++ starter/assignments.js
@@ -1,6 +1,14 @@
 let country = "Iran";
 let continent = "Asia";
 let population = 87.92;
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+let isIsland = true;
+let language;
+
+console.log(typeof country);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
+
--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js="amazing";
     if(js==="amazing"){
         alert("Js is Amazing!");
@@ -8,3 +9,19 @@ let _function = "test";
 let PI=3.141595268;
 console.log(test);
 console.log(PI);
+*/
+
+/*
+let javascriptIsfun = true;
+let userName = "ali";
+let age = 21;
+console.log(typeof javascriptIsfun);
+console.log(typeof userName);
+console.log(typeof true);
+console.log(typeof age);
+javascriptIsfun = "true";
+console.log(typeof javascriptIsfun);
+
+let testvariable;
+console.log(typeof testvariable);
+*/
