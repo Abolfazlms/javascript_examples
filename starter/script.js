67ff977a2857b4839a191b3f2587caaa7f831d6f
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-29 16:58:13 +0330

    some changes added to this project

--- starter/script.js
+++ starter/script.js
@@ -31,4 +31,22 @@ const birth_year = 1991;
 // const user_info ="the user "+firstName+" "+lastName+" birth in "+birth_year+" and he has "+(2022-birth_year)+" years old.";
 const user_info =`the user ${firstName} ${lastName} birth in ${birth_year} and he has ${2022-birth_year} years old.`;
 console.log(user_info);
+
+let number = prompt("Enter a number : ");
+console.log(number);
 */
+
+let team_1_score,team_2_score;
+team_1_scores = (25+51+32)/3 ;
+team_2_score = (21+30+60)/3;
+
+if (team_1_score > team_2_score && team_1_score >= 100){
+    console.log(`team 1 with average score ${team_1_score} is winner!`);
+}else if (team_2_score > team_1_score && team_2_score >= 100){
+    console.log(`team 2 with average score ${team_2_score} is winner!`);
+} else if (team_1_score === team_2_score && team_1_score>=100 && team_2_score>=100){
+    console.log('team score is equal and both of them wins!');
+}else{
+    console.log('the teams are equal.');
+}
+
