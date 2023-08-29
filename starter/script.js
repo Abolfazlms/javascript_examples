5850b9060da18017a6e5156fe4400f806d815638
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-29 18:00:52 +0330

    some changes added to this project

--- starter/script.js
+++ starter/script.js
@@ -34,7 +34,6 @@ console.log(user_info);
 
 let number = prompt("Enter a number : ");
 console.log(number);
-*/
 
 let team_1_score,team_2_score;
 team_1_scores = (25+51+32)/3 ;
@@ -50,3 +49,57 @@ if (team_1_score > team_2_score && team_1_score >= 100){
     console.log('the teams are equal.');
 }
 
+let day = 'sunday';
+switch(day){
+    case 'saturday':
+        console.log('first day of the week');
+        break;
+    case 'sunday':
+        console.log('second day of the week');
+        break;
+    case 'monday':
+        console.log('thirdday of the week');
+        break;
+    case 'tuesday':
+        console.log('fourth day of the week');
+        break;
+    case 'wednesday':
+        console.log('fifth day of the week');
+        break;
+    case 'thursday':
+        console.log('sixth day of the week');
+        break;
+    case 'friday':
+        console.log('seventh day of the week');
+        break;
+    default:
+        console.log('enter a valid value');
+}
+
+if(day === 'saturday'){
+    console.log('first day of the week');
+}else if (day === 'sunday'){
+    console.log('second day of the week');
+}else if (day === 'monday'){
+    console.log('thirdday of the week');    
+}else if (day === 'tuesday'){
+    console.log('fourth day of the week');    
+}else if (day === 'wednesday'){
+    console.log('fifth day of the week');    
+}else if (day === 'thursday'){
+    console.log('sixth day of the week');    
+}else if (day === 'friday'){
+    console.log('seventh day of the week');    
+}else{
+    console.log('enter a valid value');
+}
+
+let age = 21;
+let user_age = age>=18?'user age is greater than 18.':'user age smaller than 18.';
+console.log(user_age);
+
+console.log(`user age is${age>=21?' greater ':' smaller '}than 18.`);
+*/
+let bill = 275;
+let tip = 50<=bill && bill>=100 ? (bill*15)/100 : (bill*20)/100;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill+tip}`);
