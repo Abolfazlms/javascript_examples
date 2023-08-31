53f46fa4b9fd42e49202955c08125602f5f99529
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-08-31 09:58:49 +0330

    challenge1 is completed.

--- starter2/script.js
+++ starter2/script.js
@@ -35,34 +35,52 @@
 // console.log(check_winner(43,21));
 
 // const friends = new Array('ali','steve','hassan');
-const friends = ['ali','steve','hassan'];
-const birth_year = 1990;
-const money = [1000,2000,2500];
-const age_calculator = function(birth_year){
-    return 2023-birth_year;
+// const friends = ['ali','steve','hassan'];
+// const birth_year = 1990;
+// const money = [1000,2000,2500];
+// const age_calculator = function(birth_year){
+//     return 2023-birth_year;
+// }
+
+// const person = ['ali','alipour',age_calculator(birth_year),money[money.length-1],friends];
+// console.log(person);
+
+// friends.push('hossein')
+// console.log(friends);
+
+// friends.pop('hossein');
+// console.log(friends);
+
+// friends.unshift('alireza');
+// console.log(friends);
+
+// friends.shift('alireza');
+// console.log(friends);
+
+// const friend_index = friends.indexOf('ali');
+// console.log(`ali is ${friend_index+1}st of your friends`);
+
+// const isFriend = friends.includes('hossein');
+// if(isFriend){
+//     console.log('hossein is friends.');
+// }else{
+//     console.log('hossein not in friend list.');
+// }
+
+function tipCalculator(bill){
+    // if(bill>=50 && bill<=300){
+    //     return bill*0.15;
+    // }
+    // else{
+    //     return bill*0.2;
+    // }    
+    return bill>=50 && bill<=300 ? bill*0.15 : bill*0.2;
 }
+const bills = [125,55,44];
+const tips = [tipCalculator(bills[0]),tipCalculator(bills[1]),tipCalculator(bills[2])];
+const totalValues = [tips[0]+bills[0]];
+totalValues.push(bills[1]+tips[1]);
+totalValues.push(bills[2]+tips[2]);
 
-const person = ['ali','alipour',age_calculator(birth_year),money[money.length-1],friends];
-console.log(person);
-
-friends.push('hossein')
-console.log(friends);
-
-friends.pop('hossein');
-console.log(friends);
-
-friends.unshift('alireza');
-console.log(friends);
-
-friends.shift('alireza');
-console.log(friends);
-
-const friend_index = friends.indexOf('ali');
-console.log(`ali is ${friend_index+1}st of your friends`);
-
-const isFriend = friends.includes('hossein');
-if(isFriend){
-    console.log('hossein is friends.');
-}else{
-    console.log('hossein not in friend list.');
-}
+console.log(bills,tips);
+console.log(totalValues);
