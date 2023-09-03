a334cafe1369245bfc52cd700c277c0d0582c464
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-09-03 08:29:58 +0330

    some changes added to this project

--- starter3/script.js
+++ starter3/script.js
@@ -60,3 +60,49 @@
 // console.log(
 //     `the amplitude of temperaturs is ${tempAmplitudeCalculator(temperatures)}`
 // );
+
+// const measureKelvin = function () {
+//     const measurement = {
+//         type: 'temperature',
+//         unit: 'cels',
+//         // value: Number(
+//         //     prompt('Please Enter an value for temperature (Degrees celsius) :')
+//         // ),
+//         value: 21,
+//     };
+
+//     // console.log(measurement);
+//     console.table(measurement);
+
+//     console.log(measurement.value);
+//     // console.warn(measurement.value);
+//     // console.error(measurement.value);
+
+//     const kelvin_value = measurement.value + 273;
+//     return kelvin_value;
+// };
+// console.log(measureKelvin());
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 15, 4, 21, 17, 19];
+
+// function tempAmplitudeCalculator(temperatures) {
+//     debugger;
+//     let max = temperatures[0],
+//         min = temperatures[0];
+
+//     for (let i = 0; i < temperatures.length; i++) {
+//         if (typeof temperatures[i] !== 'number') {
+//             continue;
+//         }
+//         max <= temperatures[i] ? (max = temperatures[i]) : (max = max);
+//         min >= temperatures[i] ? (min = temperatures[i]) : (min = min);
+//     }
+//     console.log(
+//         `maximum temperature is ${max} , minimum temperature is ${min}`
+//     );
+//     const amplitude = max - min;
+//     return amplitude;
+// }
+// console.log(
+//     `the amplitude of temperaturs is ${tempAmplitudeCalculator(temperatures)}`
+// );
