2b0542c431edab8cac6250eedef86d179c07360b
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-09-05 08:08:37 +0330

    some changes added to this project

--- starter4/script.js
+++ starter4/script.js
@@ -1,3 +1,11 @@
 'use strict';
 // console.log(document.querySelector('.message'));
+// console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Answer!';
 console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = '12';
+
+document.querySelector('.guess').value = 21;
+console.log(document.querySelector('.guess').value);
