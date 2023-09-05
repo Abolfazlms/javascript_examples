6b94bb90acac9152988d2d17f2b332011eda49dc
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-09-05 09:28:07 +0330

    some changes added to this project

--- starter4/script.js
+++ starter4/script.js
@@ -16,7 +16,38 @@
 //     console.log('hello world!');
 // }
 
+// document.querySelector('.guess_check').addEventListener('click', function () {
+//     console.log(document.querySelector('.guess').value);
+//     document.querySelector('.message').textContent = '🎉 Correct Number!';
+// });
+
+const secret_number = Math.trunc(Math.random() * 20 + 1);
+console.log(secret_number);
+let score = 10;
+
 document.querySelector('.guess_check').addEventListener('click', function () {
-    console.log(document.querySelector('.guess').value);
-    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    const guess = Number(document.querySelector('.guess').value);
+    // console.log(guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = '🔔 Enter a Number!';
+    } else if (guess && score > 1) {
+        if (guess === secret_number) {
+            document.querySelector('.message').textContent =
+                '🎉 Correct Number!';
+            document.querySelector('.number').textContent = secret_number;
+        } else if (guess < secret_number) {
+            document.querySelector('.message').textContent =
+                '📉 Too Low Number!';
+            score -= 1;
+        } else if (guess > secret_number) {
+            document.querySelector('.message').textContent =
+                '📈 Too High Number!';
+            score -= 1;
+        }
+    } else {
+        document.querySelector('.message').textContent =
+            '💥 You Lost The Game!';
+        score = 0;
+    }
+    document.querySelector('.score').textContent = score;
 });
