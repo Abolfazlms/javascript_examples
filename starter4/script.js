200e9d73e3a791e0f123e2aa5669b22fb409a8a7
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-09-05 08:37:38 +0330

    some changes added to this project

--- starter4/script.js
+++ starter4/script.js
@@ -2,10 +2,21 @@
 // console.log(document.querySelector('.message'));
 // console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Correct Answer!';
-console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Answer!';
+// console.log(document.querySelector('.message').textContent);
 
 // document.querySelector('.number').textContent = '12';
 
-document.querySelector('.guess').value = 21;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 21;
+// console.log(document.querySelector('.guess').value);
+
+// document.querySelector('.guess_check').addEventListener('click', hello);
+
+// function hello() {
+//     console.log('hello world!');
+// }
+
+document.querySelector('.guess_check').addEventListener('click', function () {
+    console.log(document.querySelector('.guess').value);
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+});
