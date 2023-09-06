7424bf4947bcbd5bc442f6684c94e8e653d314f7
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-09-06 18:03:15 +0330

    some changes added to this project

--- starter5/script.js
+++ starter5/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const openModal = function () {
+    modal.classList.remove('hidden');
+    // modal.style.display = 'block';
+    overlay.classList.remove('hidden');
+};
+// console.log(btnShowModal);
+for (let i = 0; i < btnShowModal.length; i++) {
+    // console.log(btnShowModal[i].textContent);
+    btnShowModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
