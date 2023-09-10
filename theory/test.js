796d51cbdbf737dbe191789bb3452b287323ddfb
Abolfazlms <Abolfazlms14002021@gmail.com>
2023-09-10 20:11:20 +0330

    some changes added to this project

--- theory/test.js
+++ theory/test.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const userName = 'ali'; //global scope
+
+function ageCalculator(birthYear) {
+    const age = 2023 - birthYear; //function scope
+
+    function printAge() {
+        const output = `The user ${userName}, age is ${age} and born is ${birthYear}`;
+        console.log(output);
+
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true; //function scope
+            const userName = 'steve';
+            const str = `user ${userName}, is a millenial!`; //block scope
+            console.log(str);
+        }
+        // console.log(str);
+        console.log(millenial);
+    }
+    printAge();
+    return age;
+}
+
+ageCalculator(1993);
+// console.log(age);
+// printAge();
